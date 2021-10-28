cee759c0822706a7b9227b0e881002ea45c08fa5
circunferenzoide <miligiudice@gmail.com>
2021-10-28 12:30:51 -0300

    Pruebas agregadas

--- src/CarritodeCompras.java
+++ src/CarritodeCompras.java
@@ -4,12 +4,14 @@ public class CarritodeCompras {
     private String detalle;
     private int cantidad;
     private String nombre;
+    private float preciototal;
 
-    public CarritodeCompras(float precio, String detalle, int cantidad, String nombre) {
+    public CarritodeCompras(float precio, String detalle, int cantidad, String nombre, float preciototal) {
         this.precio = precio;
         this.detalle = detalle;
         this.cantidad = cantidad;
         this.nombre = nombre;
+        this.preciototal = preciototal;
     }
 
     public int reducircantidad (int cantidad, int c){
@@ -24,5 +26,12 @@ public class CarritodeCompras {
         int d = cantidad;
         d = d + c;
         return d;
+    }
+
+    public float modificar_precio (int precio, float preciototal, String nombre, int cantidad){
+        float totalcalculado = cantidad * precio;
+        preciototal = totalcalculado;
+        System.out.println("El precio total es: "+ preciototal);
+        return preciototal;
     }
 }
--- src/CarritodeComprasTest.java
+++ src/CarritodeComprasTest.java
@@ -6,15 +6,31 @@ class CarritodeComprasTest {
 
     @Test //reduce la cantidad del producto en 1 y que la cantidad siga siendo mayor a 0
     void reducircantidadproducto(){
-       CarritodeCompras C1 = new CarritodeCompras(1500, "Negro", 3,"Equipo_de_Musica");
+       CarritodeCompras C1 = new CarritodeCompras(1500, "Negro", 3,"Equipo_de_Musica",4500);
 
        assertEquals(2, C1.reducircantidad(3,1));
     }
 
     @Test //se aumenta la cantidad del producto en 1
     void aumentarcantidadproducto(){
-        CarritodeCompras C1 = new CarritodeCompras(1500, "Negro", 3,"Equipo_de_Musica");
+        CarritodeCompras C1 = new CarritodeCompras(1500, "Negro", 3,"Equipo_de_Musica",4500);
 
         assertEquals(4, C1.aumentarcantidad(3,1));
+    }
+
+    @Test //se reduce la cantidad de un producto en 1 y se modifica el precio total
+    void modificarprecioaldisminuircantidad(){
+        CarritodeCompras C1 = new CarritodeCompras(1500, "Negro", 3,"Equipo_de_Musica", 4500);
+
+        assertEquals(3000, C1.modificar_precio(1500, 4500,
+                "Equipo de Musica", C1.reducircantidad(3,1)));
+    }
+
+    @Test //se aumenta el total del precio al aumentar el producto
+    void modificarprecioalaumentarproducto(){
+        CarritodeCompras C1 = new CarritodeCompras(1500, "Negro", 3,"Equipo_de_Musica", 4500);
+
+        assertEquals(6000, C1.modificar_precio(1500, 4500, "Equipo de Musica",
+                C1.aumentarcantidad(3,1)));
     }
 }
